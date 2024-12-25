9dc378ed0918b55601250b75f485bbba62dec9ef
bojan.calic <bojan.calic@protonmail.ch>
2024-12-25 16:09:18 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Define aliases for cleaner imports
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-
-  // Vite's test configuration for Jest
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
-    exclude: ['**/node_modules/**', '**/dist/**'],
-  },
-
-
-  // Server configuration
-  server: {
-    open: true,
-    port: 3000,
-  },
-
-  // Build options
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
+    include: ['components/**/*.{test,spec}.{js,jsx,ts,tsx}', 'src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    exclude: ['node_modules', 'dist'],
   },
 });
